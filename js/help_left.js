8426c2a946a9930a9b1edf219cd314847af849e6
Anna <245369126@qq.com>
2018-10-27 06:07:48 +0800

    first commit

---
+++ js/help_left.js
@@ -0,0 +1,9 @@
+document.writeln("<div id=\'help_left\'>");
+document.writeln("			<form>");
+document.writeln("				<input type=\'button\' class=\'help_button\' id =\'help_button1\'/>");
+document.writeln("				<input type=\'button\' class=\'help_button\' id =\'help_button2\'/>");
+document.writeln("				<input type=\'button\' class=\'help_button\' id =\'help_button3\'/>");
+document.writeln("				<input type=\'button\' class=\'help_button\' id =\'help_button4\'/>");
+document.writeln("");
+document.writeln("			</form>");
+document.writeln("		</div>");
